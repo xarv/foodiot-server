61fb3364abf152e4205080ef4fdff17503550b2a
Harshit Pathak <hapatha@Harshits-MacBook-Pro-2.local>
2018-07-25 11:32:00 +0530

    changing docs location

--- docs.js
+++ docs.js
@@ -2,5 +2,5 @@ const Docs = require('express-api-doc');
 const app = require('./app'); // your app.js
 const docs = new Docs(app);
 docs.generate({
-  path:     './public/docs.html',
+  path:     './build/docs.html',
 });
