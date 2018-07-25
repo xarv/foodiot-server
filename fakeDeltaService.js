bf500fcf6dea906eaf8db829386c8cbc5387c91a
Harshit Pathak <hapatha@Harshits-MBP-2.guest.corp.microsoft.com>
2018-07-25 13:17:14 +0530

    adding node fetch

--- fakeDeltaService.js
+++ fakeDeltaService.js
@@ -1,8 +1,11 @@
+const fetch = require('node-fetch')
 
 setInterval( () => {
     fetch('http://104.215.195.176/bowls/1/delta/-10')
     .then( (result) => {
-        console.log(result);
+        result.text().then(result => {
+            console.log(result);
+        })
     } )
     .catch( err => {
         console.log(err);
